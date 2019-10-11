5ac991e7db5a888cb88a268fa2103b99da1633fb
BlackSSV <blackssv@gmail.com>
2019-10-11 19:27:56 +0300

    add local/activities/universalaction

---
+++ local/activities/universalaction/.description.php
@@ -0,0 +1,12 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arActivityDescription = array(
+    "NAME" => GetMessage("BPUA_DESCR_NAME"),
+    "DESCRIPTION" => GetMessage("BPUA_DESCR_DESCR"),
+    "TYPE" => "activity",
+    "CLASS" => "UniversalAction",
+    "JSCLASS" => "BizProcActivity",
+    "CATEGORY" => array(
+        "ID" => "other",
+    ),
+);
+?>
